ca8178b33f7fe9cddd4cfb17bbb588c29d7646fa
milan3991 <m.mircic93@gmail.com>
2025-05-01 16:15:47 +0200

    navigation update

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,23 +1,12 @@
-import { useState } from "react";
 import './navigation.style.css';
 import logo from '../../assets/logo.svg';
-import burgerIcon from '../../assets/burger.svg';
 
 const Navigation = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
 
     return (
         <div className="navigation">
             <div className="navigation_wrapper">
                     <img src={logo} alt="My Portfolio Logo" className="logo" />
-
-                <button className={`burger ${menuOpen ? 'rotate' : ''}`} onClick={toggleMenu}>
-                    <img src={burgerIcon} alt="Burger menu" />
-                </button>
             </div>
         </div>
     );
